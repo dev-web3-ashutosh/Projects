6206ba5a9b6f01f97cd92690e1c32b28a5dad574
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-15 00:54:32 +0530

    define deposit method

--- Banking v1/src/com/ashutosh/service/Banking.java
+++ Banking v1/src/com/ashutosh/service/Banking.java
@@ -1,11 +1,24 @@
 package com.ashutosh.service;
 
+import java.util.Scanner;
+
 public class Banking {
 	
-	int balance, amount = 0;
+	float balance, amount = 0;
+	Scanner sc = new Scanner(System.in);
 	
 	public void deposit() {
+		System.out.println("Enter the amount to deposit");
+		amount=sc.nextFloat();
 		
+		if(amount>0) {
+			balance += amount;
+			System.out.println("Rs. " + amount + " deposited successfully");
+			System.out.println("Available balance: " + balance());
+		}
+		else {
+			System.out.println("Please enter valid amount");
+		}
 	}
 	
 	public void withdrawal() {
