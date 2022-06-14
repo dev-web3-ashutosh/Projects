9f509c711179ffa3413f08ad8845bd2fb5ff9ec3
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-14 20:38:10 +0530

    create setter method

--- Banking v1/src/com/ashutosh/model/Customer.java
+++ Banking v1/src/com/ashutosh/model/Customer.java
@@ -2,8 +2,15 @@ package com.ashutosh.model;
 
 public class Customer {
 	
+	//encapsulation
 	private long bankAccountNo;
 	private String password;
+	
+	//setter method
+	public void setCredential(long bankAccountNo, String password) {
+		this.bankAccountNo=bankAccountNo;
+		this.password=password;
+	}
 	
 	
 
