ece24f199edc4808b64710cc95c484cf85686dcb
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-14 21:41:10 +0530

    import and create object of customer class

--- Banking v1/src/com/ashutosh/main/Driver.java
+++ Banking v1/src/com/ashutosh/main/Driver.java
@@ -1,8 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.model.Customer;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		Customer cust1 = new Customer(123456, "password");
 		
 		
 
