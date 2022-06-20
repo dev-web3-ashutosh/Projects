1137b8b9e5752667a17eb43f641bf1a24b32b974
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-20 23:37:43 +0530

    define class employee

--- Email Generator/src/com/ashutosh/model/Employee.java
+++ Email Generator/src/com/ashutosh/model/Employee.java
@@ -1,5 +1,20 @@
 package com.ashutosh.model;
 
 public class Employee {
+	
+	private String firstName, lastName;
+
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
 
 }
