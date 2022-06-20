31d6c95facd17dc5702e6d4aa0eca577edce2002
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-20 23:53:33 +0530

    create object of employee class

--- Email Generator/src/com/ashutosh/main/Driver.java
+++ Email Generator/src/com/ashutosh/main/Driver.java
@@ -1,9 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.model.Employee;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
+		Employee emp = new Employee("Ashutosh", "Tripathi");
 		
 
 	}
