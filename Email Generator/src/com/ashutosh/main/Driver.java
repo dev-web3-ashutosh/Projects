fc8dcf278032ad58c3ddfb4ab1868b17252a9e57
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-20 23:54:23 +0530

    create object of class credentialservice

--- Email Generator/src/com/ashutosh/main/Driver.java
+++ Email Generator/src/com/ashutosh/main/Driver.java
@@ -1,12 +1,15 @@
 package com.ashutosh.main;
 
 import com.ashutosh.model.Employee;
+import com.ashutosh.service.CredentialService;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		
 		Employee emp = new Employee("Ashutosh", "Tripathi");
+		CredentialService cs = new CredentialService();
+		
 		
 
 	}
