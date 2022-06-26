28e15628372083e2f625721a2c160229d86aa3ab
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-26 12:07:02 +0530

    create GUI elements

--- GUI Calculator/src/com/ashutosh/service/Calculator.java
+++ GUI Calculator/src/com/ashutosh/service/Calculator.java
@@ -1,5 +1,33 @@
 package com.ashutosh.service;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 public class Calculator {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextArea textArea = new JTextArea();
+	
+	JButton button0 = new JButton();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
+	JButton button5 = new JButton();
+	JButton button6 = new JButton();
+	JButton button7 = new JButton();
+	JButton button8 = new JButton();
+	JButton button9 = new JButton();
+	
+	JButton buttonAdd = new JButton();
+	JButton buttonSub = new JButton();
+	JButton buttonMul = new JButton();
+	JButton buttonDiv = new JButton();
+	JButton buttonClear = new JButton();
+	JButton buttonDot = new JButton();
+	JButton buttonEqual = new JButton();
 
 }
