6b30facee064ecb021e5281ec1da793cf09b6a0a
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-26 11:22:26 +0530

    define method rollDice

--- SixSidedDie/src/RollingDice.java
+++ SixSidedDie/src/RollingDice.java
@@ -1,6 +1,12 @@
 import java.util.Random;
 
 public class RollingDice {
+	
+	Random rand = new Random();
+	
+	public int rollDice() {
+		return rand.nextInt(6) + 1;
+	}
 
 	public static void main(String[] args) {
 		
