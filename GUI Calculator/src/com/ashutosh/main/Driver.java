cae8da9748e70e4285ffa5a7bf54ab11e1f14beb
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-26 12:44:48 +0530

    create object of class Calculator in main

--- GUI Calculator/src/com/ashutosh/main/Driver.java
+++ GUI Calculator/src/com/ashutosh/main/Driver.java
@@ -1,9 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.service.Calculator;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
+		Calculator calc = new Calculator();
 		
 
 	}
