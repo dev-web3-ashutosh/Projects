f3d0de116b8b744db50b4df8c789b12c0ab396df
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-26 11:33:35 +0530

    create object of class RollingDice

--- SixSidedDie/src/com/ashutosh/main/Driver.java
+++ SixSidedDie/src/com/ashutosh/main/Driver.java
@@ -1,8 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.service.RollingDice;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		RollingDice obj = new RollingDice();
 		
 		
 
